e75cf26b474f334fefc2880c27094ce31ae56001
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-29 13:11:44 +0200

    Add project files.

---
+++ Uppgift 3.13 Josef/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+namespace Uppgift_3._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            Console.ReadLine();
+            Console.WriteLine("Jag är 17 år gammal");
+            Console.ReadLine();
+            Console.WriteLine("Du får vara med i tävlingen");
+        }
+
+
+    }
+}
